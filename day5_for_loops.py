5c1bcd3cf3b53dbc103ffd26d733258f61ef35c0
Lelouch <lelouchocean@gmail.com>
2025-09-17 22:36:32 +0800

    add day5_homework and modify the day5_for_loops

--- day5_for_loops.py
+++ day5_for_loops.py
@@ -13,3 +13,30 @@ for todo in todos:
 print(f"{"----------------------":^{total_wd}}")
 print("\n")
 print("---")
+
+# add the number before the tasks
+
+print("-----今天的待办事项-----")
+for todo in todos:
+    full_td = f'\u2022 {todo}'
+    print(full_td)
+print("----------------------")
+print("\n")
+
+num_task= 1
+print('-----今天的待办事项-----')
+for todo in todos:
+    print(f"\u2022 {num_task}.{todo}")
+    num_task += 1
+print('----------------------')
+
+# add remark
+num_task= 1
+print('-----今天的待办事项-----')
+for todo in todos:
+    if "Python" in todo or "GitHub" in todo:
+        print(f"\u2022 【重要】{num_task}.{todo}")
+    else:
+        print(f"\u2022 【普通】{num_task}.{todo}")
+    num_task += 1
+print('----------------------')
---
+++ day5_homework.py
@@ -0,0 +1,6 @@
+# 计算总分
+scores = [95, 88, 92, 79, 85]
+num = 0
+for score in scores:
+    num += score
+print(f"最后的总分是{num}")
