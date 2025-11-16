486aae129dfc2b317464d44185233d8ef83d2865
Lelouch <lelouchocean@gmail.com>
2025-11-16 23:10:08 +0800

    Add Flask courses

--- my_first_flask_app/app.py
+++ my_first_flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  # import flask class
+from flask import Flask, render_template, request, url_for, redirect  # import flask render_template request url_for and redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,18 +12,73 @@ def user_profile(name):
     """Renders a dynamic page with data passed into the templates."""
     # 1. 准备要传递给模版的数据
     user_tasks = [
-        "Complete Day 25 Python course",
-        "Practice Flask templating",
-        "Prepare for Day 26"
+        "task1",
+        "task2",
+        "task3"
     ]
     # 2. calling render_template, pass the template name and data
     # 'username=name' means that there is a variable named as 'username' in the template, and its value is that of the variable named as 'name' in Python.
     # 'tasks = user_tasks' is the same as that above
-    return  render_template(
+    if name.lower() == "admin":
+        admin = True
+    else:
+        admin = False
+
+    return render_template(
         'user_profile.html',
-        username = name,
-        tasks = user_tasks
-    )
+                            username = name,
+                            tasks = user_tasks,
+                            is_admin = admin
+                            )
+
+@app.route("/contact",methods=["GET","POST"])
+def contact():
+    # judge the method of request:
+    if request.method == "POST":
+        #This is a POST request, we need to deal with a form data
+        visitor_name = request.form["visitor_name"]
+        visitor_message = request.form["visitor_message"]
+
+        # In the real app instance, you will store the data into database. However, we only print it to show that we received the request.
+        print("--- New Message Received ---")
+        print(f"From: {visitor_name}")
+        print(f"Message: {visitor_message}")
+        print("----------------------------")
+        #redirect to the "Thank_you" page after dealing with the data
+        return redirect(url_for("thank_you"))
+    # Otherwise, the required method is GET, and we only need to show the form page.
+    return render_template("contact.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        click_action = request.form.get("action")
+        if click_action == "login":
+            login_name = request.form["login_name"]
+            login_password = request.form["login_password"]
+            print("--- Login information ---")
+            print(f"Login name is {login_name}")
+            print(f"Login Password is {login_password}")
+            return redirect(url_for("thank_you"))
+
+        elif click_action == "forget":
+            print("--- Forget information ---")
+            print("custom forget the login name and password.")
+            return redirect(url_for("home"))
+
+        elif click_action == "message":
+            message_text = request.form.get("login_remark")
+            login_name = request.form.get("login_name", "anonymous")
+            print(f"Form: {login_name}")
+            print(f"Message: {message_text}")
+            return redirect(url_for("home"))
+
+    return render_template("login.html")
+
+@app.route("/thank-you")
+def thank_you():
+    return render_template("thank_you.html")
+
 
 @app.route("/show")
 def show():
